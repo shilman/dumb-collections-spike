1a39d3609423d021b696c91ddf1a7d7766209553
Michael Shilman <shilman@lab80.co>
2014-10-24 02:08:19 +0900

    Super stripped-down example

---
+++ README.md
@@ -0,0 +1,1 @@
+Stripped down test of Richard Silverton's dumb collections package for Meteor: [richsilv:meteor-dumb-collections](https://github.com/richsilv/meteor-dumb-collections)
---
+++ dumb.js
@@ -0,0 +1,26 @@
+Dumb = new DumbCollection('dumber')
+
+if (Meteor.isClient) {
+  Template.dumbTemplate.helpers({
+    items: function () {
+      return Dumb.find()
+    }
+  });
+
+  Template.dumbTemplate.events({
+    'click button': function () {
+      Dumb.sync({failCallback: function(err) { console.log('FAIL', err); }});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    collection = Dumb._collection  
+    if(!collection.find().count()) {
+      collection.insert({name: 'foo'}) //_id: 'foo',  _id: collection._makeNewID(), 
+      collection.insert({name: 'bar'})
+      collection.insert({name: 'baz'})
+    }
+  });
+}
